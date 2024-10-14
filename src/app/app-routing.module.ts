5d1ca7cca721a012ad6ceb2d282d7ca6ac34df47
RenanCostaF <renancostaf@souunisuam.com.br>
2024-10-13 21:39:50 -0300

    Projeto Mobile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,30 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'novo-orcamento',
+    loadChildren: () => import('./novo-orcamento/novo-orcamento.module').then(m => m.NovoOrcamentoPageModule)
+  },
+  {
+    path: 'orcamento',
+    loadChildren: () => import('./orcamento/orcamento.module').then(m => m.OrcamentoPageModule)
+  },
+  {
+    path: 'andamento',
+    loadChildren: () => import('./andamento/andamento.module').then(m => m.AndamentoPageModule)
+  }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
